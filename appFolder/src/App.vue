c60237a27ff6bb1ae7f8d33b0c7570c052258168
devmanzura <niyazovamanzura36@gmail.com>
2023-05-25 21:42:49 +0500

    commit new files

---
+++ appFolder/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: 22px;
+  background-color: rgb(32, 32, 32, 0.5);
+}
+
+nav a {
+  text-decoration: none;
+  color: white;
+  margin-left: 15px;
+}
+</style>
---
+++ appFolder/src/components/TheWelcome.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="img-div">
+        <img :src="getImg" alt="Card image" />
+      </div>
+      <div class="content">
+        <b class="title">{{ title }}</b>
+        <p class="subtitle">{{ subtitle }}</p>
+        <div class="footer">
+          <div class="left">
+            <span class="owner-img"
+              >🎅
+              <!-- owner.img -->
+            </span>
+            <small>{{ owner.name }}</small>
+            <small>{{ owner.date }}</small>
+          </div>
+
+          <div class="right">👁 {{ owner.views }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OwnerCard",
+  props: {
+    imgIndex: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: false,
+      default: "Default title",
+      validate: (value) => value.length > 5,
+    },
+    subtitle: {
+      type: String,
+      required: false,
+      default: "Default subtitle",
+      validate: (value) => value.length > 5,
+    },
+    owner: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    getImg() {
+        return require(`../assets/images/grid${this.imgIndex}.jpg`)
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+.img-div {
+  width: 100%;
+  height: 150px;
+  background-color: gray;
+}
+
+img {
+    width: 100%;
+    height: 100%;
+}
+
+.card {
+  background-color: lightgray;
+  text-align: left;
+
+
+}
+
+.content {padding: 10px;}
+
+.title {font-size: 1.3em;}
+
+.subtitle {font-size: 0.8em;}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  font-size: 0.8em;
+  gap: 3px;
+
+ 
+}
+
+.footer>div {
+    display: flex;
+    flex-flow: wrap row;
+  }
+
+  .footer>.left small {margin-left: 5px;}
+
+  .footer>.right {font-size: 1.5em;}
+
+  .footer>.left .owner-img {font-size: 1.2em;}
+</style>
